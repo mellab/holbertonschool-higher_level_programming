156c5d793d004278d4f247458771dce7cc9634c1
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-11-25 16:20:30 -0500

    Makes a script that creates a table second_table in the database hbtn_0c_0 in a MySQL server and add multiples rows - closes #134

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- Creates a table second_table in the database hbtn_0c_0 in a MySQL server and
+-- add multiple rows.
+
+CREATE TABLE IF NOT EXISTS second_table (
+       id INT,
+       name VARCHAR(256),
+       score INT
+);
+
+INSERT into second_table (id, name, score)
+VALUES
+	('1','John','10'),
+	('2','Alex','3'),
+	('3','Bob','14'),
+	('4','George','8');
