f4a3626bbb73e5f4a1b77e3f386dd4ffbc92c8e6
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-11-26 13:24:18 -0500

    Makes a script that creates the database hhbtn_0d_usa and the table states (in the database hbtn_0d_usa) on a MySQL server - closes #152

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_usa and the table states (in the database
+-- hbtn_0d_usa) on a MySQL server.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(256) NOT NULL
+);
